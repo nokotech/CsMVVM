48b5e37f1f77884cf5106125ded8116d2d034b09
Takenoko <takenoko.tech@gmail.com>
2018-11-24 13:50:05 +0900

    fix mock.

--- mock/server.mjs
+++ mock/server.mjs
@@ -1,71 +1,100 @@
 import express from "express";
+import morgan from "morgan";
+import log4js from "log4js";
 import bodyParser from "body-parser";
-import ObjectID from "mongodb";
-import mongo from "./mongo.mjs";
+import mongo, { objID } from "./mongo.mjs";
 
-var app = express();
+const logger = log4js.getLogger("system");
+logger.level = "debug";
+
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(log4js.connectLogger(logger, { level: "auto" }));
 app.listen(3000);
 
 const SAMPLE = "sample";
 
 app.get("/sample", (req, res) => {
-  mongo(SAMPLE).then(collection => {
-    collection.find().toArray((err, docs) => {
-      res.json(docs);
+    mongo(SAMPLE).then(collection => {
+        logger.debug(req.params.id, req.body);
+        collection
+            .find(req.body)
+            .toArray()
+            .then(r => {
+                res.json(r);
+            })
+            .catch(err => {
+                logger.error(err);
+                res.send(err);
+            });
     });
-  });
+});
+
+app.get("/sample/:id", (req, res) => {
+    mongo(SAMPLE).then(collection => {
+        logger.debug(req.params.id, req.body);
+        collection
+            .findOne({ _id: objID(req.params.id) })
+            .then(r => {
+                res.json(r);
+            })
+            .catch(err => {
+                logger.error(err);
+                res.send(err);
+            });
+    });
 });
 
 app.post("/sample", (req, res) => {
-  mongo(SAMPLE).then(collection => {
-    let row = req.body || {};
-    console.log(row);
-
-    collection
-      .insertOne(row)
-      .then(r => {
-        res.json(r);
-      })
-      .catch(err => {
-        console.error(err);
-        res.send(err);
-      });
-  });
+    mongo(SAMPLE).then(collection => {
+        logger.debug(req.body);
+        let row = req.body || {};
+        if (!row.name) {
+            res.send("validate error.");
+        }
+        collection
+            .insertOne(row)
+            .then(r => {
+                res.json(r);
+            })
+            .catch(err => {
+                logger.error(err);
+                res.send(err);
+            });
+    });
 });
 
 app.put("/sample/:id", (req, res) => {
-  mongo(SAMPLE).then(collection => {
-    console.log(req.params.id, req.body);
-    collection
-      .findOneAndUpdate(
-        { _id: req.params.id },
-        { $set: req.body },
-        { sort: { points: 1 }, upsert: true, returnNewDocument: true }
-      )
-      .then(r => {
-        res.json(r);
-      })
-      .catch(err => {
-        console.error(err);
-        res.send(err);
-      });
-  });
+    mongo(SAMPLE).then(collection => {
+        logger.debug(req.params.id, req.body);
+        collection
+            .findOneAndUpdate(
+                { _id: objID(req.params.id) },
+                { $set: req.body },
+                { sort: { points: 1 }, upsert: true, returnNewDocument: true }
+            )
+            .then(r => {
+                res.json(r);
+            })
+            .catch(err => {
+                logger.error(err);
+                res.send(err);
+            });
+    });
 });
 
 app.delete("/sample/:id", (req, res) => {
-  mongo(SAMPLE).then(collection => {
-    console.log(req.params.id, req.body);
-    collection
-      .findOneAndDelete({ _id: req.params.id }, {})
-      .then(r => {
-        console.error(r);
-        res.json(r);
-      })
-      .catch(err => {
-        console.error(err);
-        res.send(err);
-      });
-  });
+    mongo(SAMPLE).then(collection => {
+        logger.debug(req.params.id, req.body);
+        collection
+            .findOneAndDelete({ _id: objID(req.params.id) }, {})
+            .then(r => {
+                res.json(r);
+            })
+            .catch(err => {
+                logger.error(err);
+                res.send(err);
+            });
+    });
 });
